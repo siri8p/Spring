bb703bf5cfb62c6b92b42dc5c235daeb4358ee8e
siri8p <siri8p@gmail.com>
2025-08-28 21:51:57 -0700

    add tothe target folder

--- Alldepartment.java
+++ Alldepartment.java
@@ -2,6 +2,8 @@ package com.example.Spring;
 
 public class Alldepartment {
     public static void main(String[] args) {
+
         System.out.println("it is alldepartments i added");
+        System.out.println("target folder add");
     }
 }
